2959b340570a0073202e3089bd3eb7bbfd6cba46
Promal B <promal.barua.cse@gmail.com>
2021-07-27 20:21:57 +0600

    Initial Commit

---
+++ DrawingPractice/DrawingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingPracticeApp.swift
+//  DrawingPractice
+//
+//  Created by Promal on 27/7/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
